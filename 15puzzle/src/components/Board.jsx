051e9bdb16cf4270706ff4fb1ca4ff7a50fc44ff
Deividas R <deividas@Deividass-MacBook-Pro.local>
2020-06-21 14:15:22 -0400

    * Implemented basic cell grid with cell switching

--- 15puzzle/src/components/Board.jsx
+++ 15puzzle/src/components/Board.jsx
@@ -4,11 +4,82 @@ import Cell from "./Cell";
 class Board extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      cells: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, -1], // -1 represents the empty cell
+    };
   }
 
+  // shuffleCells = () => {
+  //   let newCells = [...this.state.cells];
+  //   // fisher-yates shuffle
+  //   let m = array.length,
+  //     t,
+  //     i;
+  //   // While there remain elements to shuffle…
+  //   while (m) {
+  //     // Pick a remaining element…
+  //     i = Math.floor(Math.random() * m--);
+  //     // And swap it with the current element.
+  //     t = newCells[m];
+  //     newCells[m] = newCells[i];
+  //     newCells[i] = t;
+  //   }
+  //   this.setState({
+  //     cells: newCells,
+  //   });
+  // };
+
+  handleClickCell = (index) => {
+    let localCells = [...this.state.cells];
+    let tempCell = null;
+    // check top
+    if (index - 4 >= 0 && localCells[index - 4] === -1) {
+      tempCell = localCells[index - 4];
+      localCells[index - 4] = localCells[index];
+      localCells[index] = tempCell;
+    }
+    // check bottom
+    else if (index + 4 < 16 && localCells[index + 4] === -1) {
+      tempCell = localCells[index + 4];
+      localCells[index + 4] = localCells[index];
+      localCells[index] = tempCell;
+    }
+    // check left
+    else if (index - 1 >= 0 && localCells[index - 1] === -1) {
+      tempCell = localCells[index - 1];
+      localCells[index - 1] = localCells[index];
+      localCells[index] = tempCell;
+    }
+    // check right
+    else if (index + 1 < 16 && localCells[index + 1] === -1) {
+      tempCell = localCells[index + 1];
+      localCells[index + 1] = localCells[index];
+      localCells[index] = tempCell;
+    }
+    // one above condition was true
+    if (tempCell != null) {
+      this.setState({
+        cells: localCells,
+      });
+    }
+  };
+
   render() {
-    return <div></div>;
+    return (
+      <div className="board">
+        {this.state.cells.map((el, index) => {
+          return (
+            <Cell
+              key={"cell" + el}
+              index={index}
+              num={el}
+              // className={el !== -1 ? "board-cell" : "empty-cell"}
+              handleClickCell={this.handleClickCell}
+            />
+          );
+        })}
+      </div>
+    );
   }
 }
 
--- 15puzzle/src/components/Cell.jsx
+++ 15puzzle/src/components/Cell.jsx
@@ -1,7 +1,25 @@
 import React from "react";
 
-const Cell = () => {
-  return <div></div>;
+const Cell = (props) => {
+  console.log(props);
+  return (
+    <div
+      num={props.num}
+      index={props.index}
+      // className={
+      //   props.num !== -1 ? "board-cell" : "board-cell board-cell-empty"
+      // }
+      style={
+        props.num === -1
+          ? { backgroundColor: "white", color: "white" }
+          : { backgroundColor: "pink", color: "black" }
+      }
+      className={"board-cell"}
+      onClick={() => props.handleClickCell(props.index)}
+    >
+      {props.num}
+    </div>
+  );
 };
 
 export default Cell;
